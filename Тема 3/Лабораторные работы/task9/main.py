d8f7209ac39ffeb7fd6e425a84f020f011cda197
RinaEastwind <RinaEastwind@users.noreply.github.com>
2023-03-21 17:52:38 +0500

    21/03

--- Тема 3/Лабораторные работы/task9/main.py
+++ Тема 3/Лабораторные работы/task9/main.py
@@ -3,8 +3,15 @@ spend = 6000  # траты
 months = 10  # количество месяцев
 increase = 0.03  # рост цен
 
-need_money = 0  # количество денег, чтобы прожить 10 месяцев
-
+salary_10months = salary*months
+spend_10month = (spend*increase+spend)*months
+budget = salary_10months-spend_10month
+if budget < 0:
+    need_money = abs(budget)+salary_10months  # количество денег, чтобы прожить 10 месяцев
+    print("Заработанных денег не хватит. Нужно иметь накопления в размере %s. Общее количество денег, нужное, чтобы прожить 10 месяцев, составляет %s" % (round(abs(budget)), round(need_money)))
+else:
+    need_money = salary_10months - spend_10month
+    print("Общее количество денег, нужное, чтобы прожить 10 месяцев, составляет %s" % round(need_money))
 # TODO Оформить решение
 
-print(round(need_money))
+#print(round(need_money))
