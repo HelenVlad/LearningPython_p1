583e54ba80d0c9565822689a4e0d3d124018a315
RinaEastwind <RinaEastwind@users.noreply.github.com>
2023-03-21 17:26:07 +0500

    21/03

--- Тема 3/Лабораторные работы/task8/main.py
+++ Тема 3/Лабораторные работы/task8/main.py
@@ -1,10 +1,20 @@
 money_capital = 10000
-salary = 5000
-spend = 6000
-increase = 0.05
+salary = 5000 # зарплата
+spend = 6000 # траты
+increase = 0.05  # увеличение чего?
 
-month = 0  # количество месяцев, которое можно прожить
+#month =   # количество месяцев, которое можно прожить
+
+for month in range(1, 101):
+    spend += spend*increase # если имеется ввиду увеличение трат
+    money_capital=money_capital+salary-spend
+    if money_capital > 0:
+        print("В %s по счету месяце еще живем. Накопления составляют %s" % (month, round(money_capital,2)))
+    else:
+       print("В %s по счету месяце накопления закончились. Может стоит сменить работу?" % month)
+       break
+
 
 # TODO Оформить решение
 
-print(month)
+#print(month)
