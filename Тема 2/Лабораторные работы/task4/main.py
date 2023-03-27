6be1f66e1ca8496dd2231e85af9f7f8abb729859
RinaEastwind <RinaEastwind@users.noreply.github.com>
2023-03-27 18:44:08 +0500

    27/03

--- Тема 2/Лабораторные работы/task4/main.py
+++ Тема 2/Лабораторные работы/task4/main.py
@@ -1,14 +1,14 @@
 BYTES_ONE_CHAR = 1  # размер одного символа в байтах
 
 # никаких магических чисел
-pages = 10 # TODO ввести количество страниц
+pages = 100 # TODO ввести количество страниц
 lines = 50   # TODO ввести количество строк
-chars = 150   # TODO ввести количество символов в строке
+chars = 25   # TODO ввести количество символов в строке
 
 total_chars = chars*lines*chars  # TODO общее количество символов в книге
 total_bytes = total_chars*BYTES_ONE_CHAR  # TODO размер одной книги в байтах
 
-disk_size = 1457664  # TODO размер дискеты в байтах
+disk_size = 1440000  # TODO размер дискеты в байтах
 
 print(disk_size//total_bytes)     # TODO найти количество книг, которое поместится на дискете
 
