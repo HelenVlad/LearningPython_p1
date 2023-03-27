b1775bf7720174a2bc07faddb5bf982caf7a0a3e
RinaEastwind <RinaEastwind@users.noreply.github.com>
2023-03-27 15:25:59 +0500

    27/03

--- Тема 3/Лабораторные работы/task1/main.py
+++ Тема 3/Лабораторные работы/task1/main.py
@@ -1,7 +1,15 @@
 src = not False and True or False and not True
 
 # TODO расписать упрощение выражения
+# 1. Упростить оператор `not`
+result = True and True or False and False
 
-result = False or True # TODO подставить результат выражения
+# 2. Упростить оператор `and`
+result = True or False
+
+# 3. Упростить оператор `or`
+result = True
+
+# TODO подставить результат выражения
 
 print(src == result)
