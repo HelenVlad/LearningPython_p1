072b9b8fddb426070a5a48aaafdbfdfc2803f1fa
HelenVG <eilnw@mail.ru>
2023-04-06 16:42:01 +0500

    06/04

--- Тема 5/Лабораторная работа/task3/main.py
+++ Тема 5/Лабораторная работа/task3/main.py
@@ -1,7 +1,15 @@
+from random import shuffle, randint
 def get_unique_list_numbers() -> list[int]:
-    ...  # TODO написать функцию для получения списка уникальных целых чисел
+# TODO написать функцию для получения списка уникальных целых чисел
+    list_ = list(range(-10, 11))
+    shuffle(list_)
+    while True:
+        list_.pop(randint(0, len(list_)-1))
+        if len(list_) == 15:
+            break
+    return list_
 
-
+print(get_unique_list_numbers())
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
