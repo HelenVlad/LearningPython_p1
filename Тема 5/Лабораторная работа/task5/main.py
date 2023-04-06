422d4cfcbc2a19c3d495f5a43e183495e79f0eae
HelenVG <eilnw@mail.ru>
2023-04-06 19:07:42 +0500

    06/04

--- Тема 5/Лабораторная работа/task5/main.py
+++ Тема 5/Лабораторная работа/task5/main.py
@@ -1,5 +1,10 @@
-def get_random_password() -> str:
-    ...  # TODO написать функцию генерации случайных паролей
+from string import ascii_letters, digits
+from random import sample
 
+lst = ascii_letters + digits
+def get_random_password(n = 8) -> str:
+# TODO написать функцию генерации случайных паролей
+    password = sample(lst, n)
+    return password
 
 print(get_random_password())
