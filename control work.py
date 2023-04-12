c3cc30b8fd23eef03e30d9ad95b777746f41261c
HelenVG <eilnw@mail.ru>
2023-04-12 18:23:39 +0500

    12/04

--- control work.py
+++ control work.py
@@ -3,7 +3,7 @@ def dict_to_list(dict): # преобразует словарь с одним э
     items = (items[0], items[1])
     return items
 def number_of_cells():
-    field_size = ''.join(input('Здравствуйте, игроки! Вы запустили игру крестики-нолики. Действие игры проиходит на квадратном поле из ячеек размера N x N. Введите число N в диапазоне от 01 до 10:\n=>').split())
+    field_size = ''.join(input('Здравствуйте, игроки! Вы запустили игру крестики-нолики. Действие игры происходит на квадратном поле из ячеек размера N x N. Введите число N в диапазоне от 01 до 10:\n=>').split())
     lst = str(list(range(1, 11)))
     if (len(field_size) == 1 or len(field_size) == 2) and (field_size in lst):
         return int(field_size)
@@ -36,75 +36,72 @@ def field(cells, number_of_cells): #функция рисующая поле
 
 def players_marker(): #игрок выбирает себе маркер
     marker = ''.join(input('Какой маркер возьмет себе Player1? Если "Х" - введите 1, если "0", то 2. Невыбранный символ присвоится Player2 автоматически. \n=>').split())
-    players_marker_list = {}
+    players_marker_list = []
     if len(marker) == 1 and (marker == '1' or marker == '2'):
-            players_marker_list['Player1'] = 'X' if marker == '1' else '0'
-            players_marker_list['Player2'] = '0' if marker == '1' else 'X'
+            players_marker_list.append(('Player1', 'X')) if marker == '1' else ('Player1', '0')
+            players_marker_list.append(('Player2', '0')) if marker == '1' else ('Player2', 'X')
     else:
         print("Нет варианта ответа для выбранного значения. Выберите, пожалуйста, цифру от 1 до 2.")
         players_marker()
 
-    return players_marker_list # словарь, где хранится 2 значения в виде наименование_игрока:маркер
+    return players_marker_list # список, в котором хранится 2 кортежа в виде (наименование_игрока, маркер)
 
-def first_move(players_marker): # игрок выбирает кто ходит первый
+def first_move(players_marker): #игрок выбирает кто ходит первый
     move = ''.join(input('Кто ходит первый? Введите 1, если "Player1", если "Player2" то 2. \n=>').split())
-    first_move_list = {}
+    first_move_list = []
     if len(move) == 1 and (move == '1' or move == '2'):
         if move == '1':
-            first_move_list['Player1'] = players_marker['Player1']
+            first_move_list = players_marker[0]
         else:
-            first_move_list['Player2'] = players_marker['Player2']
+            first_move_list = players_marker[-1] #TODO players_marker выдает неитеррируемое значение
     else:
         print("Нет варианта ответа для выбранного значения. Выберите, пожалуйста, цифру от 1 до 2.")
         first_move()
-    return first_move_list
+    return first_move_list #возвращает список, в котором кортеж вида (наименование_игрока_который_ходит_первым, маркер)
 
 def player_change(player, players_marker_list):
-    return 'Player1' if player == 'Player2' else 'Player2'
+    if player[0] == 'Player1':
+        return players_marker_list[0]
+    else:
+         return players_marker_list[-1] #TODO тут где-то ошибка еще
 
-def players_move(players_marker_list, cells):
-    items = dict_to_list(players_marker_list)
-    print(items)
-    answer = ''.join(input(f'Твой ход, {items[0]}! Выбери число, соответствующее ячейке, от 1 до {len(cells)}.\n=>').split())
+def players_move(current_player, cells):
+    answer = ''.join(input(f'Твой ход, {current_player[0]}! Выбери число, соответствующее ячейке, от 1 до {len(cells)}.\n=>').split())
     lst = str(list(range(1, len(cells)+1)))
     if (len(answer) == 1 or len(answer) == 2) and (answer in lst):
         if cells[int(answer)] == 'None':
-            cells[int(answer)] = items[-1]
+            cells[int(answer)] = current_player[-1]
             return cells
         else:
             print(f'Данная ячейка занята. Пожалуйста, выберите свободную ячейку.')
-            players_move(players_marker_list, cells)
+            players_move(current_player, cells)
     else:
         print(f'Нет варианта ответа для выбранного значения. Выберите, пожалуйста, цифру от 1 до {len(cells)}.')
-        players_move(players_marker_list, cells)
+        players_move(current_player, cells)
 
-
-
-
-# number_of_cells = number_of_cells()
-# players_marker_list = players_marker()
-# first_move = first_move(players_marker_list)
-# cells = cells(number_of_cells)
-# print(field(cells, number_of_cells))
-#
-# players_move(first_move, cells)
-# print(cells)
-# print(field(cells, number_of_cells))
-
-
-number_of_cells_ = number_of_cells() # спрашиваем о размере поля, сохр в переменную
-players_marker_list = players_marker() #спрашиваем о том, кто каким маркером играет, сохр в переменную
-first_move_ = first_move(players_marker_list) #спрашиваем, кто ходит первый,  сохр в переменную
-cells_ = cells(number_of_cells_) #создаем в памяти список с ячейками заданной длинны
-counter = 0
-while True:
-    print(field(cells_, number_of_cells_))
+def game():
+    number_of_cells_ = number_of_cells() # спрашиваем о размере поля, сохр в переменную
+    players_marker_list = players_marker() #спрашиваем о том, кто каким маркером играет, сохр в переменную
+    first_move_ = first_move(players_marker_list) #спрашиваем, кто ходит первый,  сохр в переменную
+    cells_ = cells(number_of_cells_) #создаем в памяти список с ячейками заданной длинны
+    counter = 0
     move_made = []
-    if counter = 0:
-        cells_ = players_move(first_move_, cells_) # игрок сделал ход, измененное поле записывается в переменной
-        move_made = dict_to_list(first_move_)
-    else:
-        counter +=1
-        current_player = player_change(move_made[0])
-        cells_ = players_move(current_player, cells_)
+    while True:
+        print(field(cells_, number_of_cells_))
+        if counter == 0:
+            cells_ = players_move(first_move_, cells_) # игрок сделал ход, измененное поле записывается в переменной
+            move_made = first_move_
+            counter +=1
+        else:
+            counter +=1
+            print(move_made, players_marker_list)
+            current_player = player_change(move_made, players_marker_list)
+            print(current_player)
+            cells_ = players_move(current_player, cells_)
+            move_made = current_player
+        if counter == len(cells_):
+            print(field(cells_, number_of_cells_))
+            print('Игра окончена.')
+            break
 
+game()
